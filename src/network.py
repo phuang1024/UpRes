97300c67aa66156ca4045accfa3c1d1fd2e1323b
Patrick Huang <huangpatrick16777216@gmail.com>
2023-01-29 09:03:52 -0800

    Implemented ESRGAN.

--- src/network.py
+++ src/network.py
@@ -1,15 +1,15 @@
-import cv2
 import torch
+from torch.nn import Module
 
 from constants import *
 
 
-class Conv(torch.nn.Module):
+class Conv(Module):
     """
     Convolution and some stuff.
     """
 
-    def __init__(self, in_channels, out_channels, kernel_size):
+    def __init__(self, in_channels, out_channels, kernel_size=3):
         super().__init__()
         self.conv = torch.nn.Conv2d(in_channels, out_channels, kernel_size, padding=kernel_size//2)
         self.relu = torch.nn.LeakyReLU()
@@ -20,40 +20,87 @@ class Conv(torch.nn.Module):
         return x
 
 
-class UpresNet(torch.nn.Module):
-    """
-    RGB image input and output.
-    - conv
-    - upsample
-    - conv
-    - head
-    """
-
-    def __init__(self, kernel_size=3):
+class DenseBlock(Module):
+    def __init__(self, channels, depth=3):
         super().__init__()
+        self.depth = depth
 
-        self.scale_fac = SCALE_FAC
-        self.kernel_size = kernel_size
-
-        self.upsample = torch.nn.Upsample(scale_factor=SCALE_FAC, mode="bilinear", align_corners=False)
-        self.convs = torch.nn.Sequential(
-            Conv(3, 8, kernel_size),
-            Conv(8, 16, kernel_size),
-            Conv(16, 32, kernel_size),
-        )
-        self.conv_trans = torch.nn.ConvTranspose2d(32, 32, kernel_size, padding=kernel_size//2, stride=SCALE_FAC)
-        self.convs2 = torch.nn.Sequential(
-            Conv(32, 16, kernel_size),
-            Conv(16, 8, kernel_size),
-            Conv(8, 4, kernel_size),
-        )
-        self.head = Conv(4+3, 3, 1)
+        curr_ch = channels
+        for i in range(depth):
+            setattr(self, f"conv{i}", Conv(curr_ch, curr_ch))
+            curr_ch += curr_ch
+        self.conv_out = Conv(curr_ch, channels)
 
     def forward(self, x):
-        upscale = self.upsample(x)
-        x = self.convs(x)
-        x = self.conv_trans(x, output_size=upscale.shape)
-        x = self.convs2(x)
-        x = torch.cat([upscale, x], dim=1)
-        x = self.head(x)
+        for i in range(self.depth):
+            conv = getattr(self, f"conv{i}")
+            x = torch.cat([x, conv(x)], dim=1)
+        x = self.conv_out(x)
+        return x
+
+
+class RRDB(Module):
+    """
+    Residual in residual dense block.
+    Chain of dense blocks.
+    """
+
+    def __init__(self, channels, depth=3):
+        super().__init__()
+        self.depth = depth
+
+        for i in range(depth):
+            setattr(self, f"dense_block{i}", DenseBlock(channels))
+            beta = torch.nn.Parameter(torch.ones(1))
+            beta.requires_grad = True
+            setattr(self, f"beta{i}", beta)
+        setattr(self, "beta_out", torch.nn.Parameter(torch.ones(1)))
+
+    def forward(self, x):
+        for i in range(self.depth):
+            dense_block = getattr(self, f"dense_block{i}")
+            beta = getattr(self, f"beta{i}")
+            x = beta * dense_block(x)
+        x = x * getattr(self, "beta_out")
+        return x
+
+
+class UpresNet(Module):
+    """
+    Implementation of ESRGAN
+    """
+
+    def __init__(self, block_depth=3):
+        super().__init__()
+
+        self.block_depth = block_depth
+
+        self.conv_in = torch.nn.Sequential(
+            Conv(3, 4),
+            Conv(4, 8),
+            Conv(8, 16),
+        )
+
+        for i in range(block_depth):
+            setattr(self, f"rrdb{i}", RRDB(16))
+
+        # Use number of layers to control scale factor.
+        self.upsamp = torch.nn.Sequential(
+            torch.nn.ConvTranspose2d(16, 8, 4, 2, 1),
+            torch.nn.LeakyReLU(),
+            torch.nn.ConvTranspose2d(8, 4, 4, 2, 1),
+            torch.nn.LeakyReLU(),
+        )
+
+        self.conv_out = torch.nn.Sequential(
+            Conv(4, 3, 1),
+        )
+
+    def forward(self, x):
+        x = self.conv_in(x)
+        for i in range(self.block_depth):
+            rrdb = getattr(self, f"rrdb{i}")
+            x = rrdb(x)
+        x = self.upsamp(x)
+        x = self.conv_out(x)
         return x
